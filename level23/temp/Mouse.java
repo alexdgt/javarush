5f4cae6096ccb15e8a5ade39062592f6b7d2fc46
alexdgt <alex.dgt@outlook.com>
2016-12-02 15:39:05 +0500

    Create Mouse.java

---
+++ level23/temp/Mouse.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level23.lesson13.big01;
+
+/**
+ * Created by john on 02.12.2016.
+ */
+public class Mouse
+{
+    private int x;
+    private int y;
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public Mouse(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
