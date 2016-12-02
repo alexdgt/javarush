cc9efe66f353fb8762a52c08eff8a1a43308e0db
alexdgt <alex.dgt@outlook.com>
2016-12-02 15:36:23 +0500

    Create SnakeSection.java

---
+++ level23/temp/SnakeSection.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level23.lesson13.big01;
+
+/**
+ * Created by john on 02.12.2016.
+ */
+public class SnakeSection
+{
+    private int x;
+    private int y;
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public SnakeSection(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
